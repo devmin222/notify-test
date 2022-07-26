1ea9f0048ad7c4e10b10c2eabf9e4e83b8ec42df
Devmin <devmin_222@hotmail.com>
2022-07-26 17:26:53 +0530

    Channel update

--- app/Events/EasyhealthNotification.php
+++ app/Events/EasyhealthNotification.php
@@ -31,6 +31,6 @@ class EasyhealthNotification implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('channel-name');
     }
 }
